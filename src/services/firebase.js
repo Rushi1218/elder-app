917d8a656a4ae6e2b7737284004fb05a1a07a674
Rushi <madanerushikesh52@gmail.com>
2026-02-24 21:27:40 +0530

    Add Firebase config for elder app

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0zAazj-ke9vJBPYdqHuXOxm6R3Si-kDg",
+  authDomain: "elder-app-91b3f.firebaseapp.com",
+  projectId: "elder-app-91b3f",
+  storageBucket: "elder-app-91b3f.firebasestorage.app",
+  messagingSenderId: "984363029861",
+  appId: "1:984363029861:web:f061d6d8b17b8b7413bd8c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
